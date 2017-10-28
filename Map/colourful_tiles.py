fbd07e2818447040350350e3781ad549b0d86eb4
iwishiwasaneagle <jh.ewers@gmail.com>
2017-10-28 14:39:53 +0100

    fuck

--- Map/colourful_tiles.py
+++ Map/colourful_tiles.py
@@ -1,18 +1,19 @@
 import pygame, sys, map_gen, time
 
 pygame.init()
-reso_x = 64*64/2
-reso_y = 64*64/4
-divs = reso_x/64
+reso_x = 64*16
+reso_y = 64*16
+divs = reso_x/32
 
 screen = pygame.display.set_mode((reso_x, reso_y))
+
 
 dirt = (101, 71, 31)
 concrete = (37, 37, 37)
 grass = (34, 126, 15)
 sand = (176, 166, 76)
 
-type_ = {1:(101, 71, 31), 2:(37, 37, 37), 3:(34, 126, 15), 4:(176, 166, 76), 5:((255, 0, 221)), None:(0,0,0)}
+type_ = {1:(101, 71, 31), 2:(37, 37, 37), 3:(34, 126, 15), 4:(176, 166, 76), 5:((255, 0, 221)), None:(0,0,0), 0:(255, 255, 255)}
 
 screen.fill((0,0,0))
 
@@ -23,12 +24,17 @@ power = 1.5
 number_of_different_tiles = 3 # the dictionary at the top must correspond to this and the colours too. im too tired to care so fuck off
 
 map_ = map_gen.wanking_cunt(reso_x/divs, reso_y/divs, frequency, octaves, power, number_of_different_tiles)
+
+print len(map_)
 #while(True):
 while True:
 
-    for i in range(len(map_)-1):
-        x = map_[i][0][0]*divs
-        y = map_[i][0][1]*divs
-        z = map_[i][1]
-        pygame.draw.rect(screen, type_[z], (x,y,divs,divs))
+    for x in range(0,len(map_)-1):
+        for y in range(0,len(map_[0])-1):
+            z = map_[x][y]
+            z = int(float((z)))
+            #print type_[z]
+
+
+            pygame.draw.rect(screen, type_[z], ((x+1)*divs,(y+1)*divs,divs,divs))
     pygame.display.update()
